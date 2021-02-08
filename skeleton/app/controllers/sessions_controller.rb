6c1a40e6bb670b5679a081143e411bbdfac68e1e
Brad Trick <btrick@gmail.com>
2021-02-08 14:32:54 -0500

    Add Users and Sessions Controllers

---
+++ skeleton/app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+
+    def new
+        @user = User.new
+        render :new
+    end
+
+    def create
+        @user = User.find_by_credentials(
+            params[:user][:user_name],
+            params[:user][:password]
+        )
+
+        if @user.nil?
+            render json: "Credentials were wrong", status: :unprocessable_entity
+        else
+            login!(@user)
+            redirect_to cats_url
+        end    
+    end
+end
